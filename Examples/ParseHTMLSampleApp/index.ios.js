84b75f8c3e69d9791ba3b97005ca7acfe9c82037
Simar Singh <singhs95@mcmaster.ca>
2015-04-01 21:50:22 -0400

    fixed <p> bug

--- Examples/ParseHTMLSampleApp/index.ios.js
+++ Examples/ParseHTMLSampleApp/index.ios.js
@@ -15,11 +15,12 @@ var ParseHTMLSampleApp = React.createClass({
   render: function() {
     return (
       <ParseHTML 
-          code="Hello this is <b>Sample </b> HTML text. My name is <i>SpongeBob</i>, my bestfriend is <p>Patrick Star</p>. Lets use the code tag: <code>Hello World!</code>. Here is an example of <b>Nested <i>Tags</i></b>"
+          code="Hello this is <b><i></i></b><b><i>Sample</i></b> HTML text. My name is <i>SpongeBob</i>, my bestfriend is <p>Patrick Star</p>. Lets use the code tag: <code>Hello World!</code>. Here is an example of <b><i>Tags</i></b>"
           customTagToStyle={{
                               "<p>": {fontSize: 30},
                               "<code>": [{color: "#FF7F50"}, {backgroundColor: "#000000"}]
                             }}/>
+      
     );
   }
 });
